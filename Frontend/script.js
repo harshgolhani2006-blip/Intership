13d448d2398d563eca3a6f5160fd68705b8b43e4
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-06-20 15:27:31 +0530

    Intership Project

--- Frontend/script.js
+++ Frontend/script.js
@@ -13,7 +13,7 @@ form.addEventListener("submit", async (e) => {
     domain: select.value
   };
 
-  const response = await fetch("http://localhost:5000/register", {
+  const response = awaitfetch("https://intership-d8m7.onrender.com/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
